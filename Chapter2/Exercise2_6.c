0dc4ea6537e1491737713d71fb16982a63ca4a48
pengd-ksu <pengd@ksu.edu>
2023-03-23 15:28:26 -0400

    update ex 2.6 and getbits;

--- Chapter2/Exercise2_6.c
+++ Chapter2/Exercise2_6.c
@@ -1,10 +1,15 @@
 # include <stdio.h>
 unsigned setbits(unsigned x, int p, int n, unsigned y);
-main(){
+int main() {
     unsigned x = 27;                            /* binary: 11011 */
     unsigned y = 21;                            /* binary: 10101 */
     printf("%u\n", setbits(x, 3, 2, y));        /* binary: 10111, decimal: 23 */
+    return 0;
 }
 unsigned setbits(unsigned x, int p, int n, unsigned y){
-    return (x & ~(~(~0 << n) << (p+1-n)))|(y & (~(~0 << n) << (p+1-n)));
+    // 1. set x's corresponding parts to 0s
+    // 2. keep y's corresponding part, and set the rest to 0s
+    // Then x | y
+    return (x & ~(~(~0 << n) << (p+1-n))) |
+            (y & (~(~0 << n) << (p+1-n)));
 }
--- Chapter2/getbits.c
+++ Chapter2/getbits.c
@@ -1,8 +1,9 @@
 # include <stdio.h>
 unsigned getbits(unsigned x, int p, int n);
-main(){
+int main() {
     unsigned x = 27;                            /* binary: 11011 */
     printf("%u\n", getbits(x, 3, 3));           /* should be 101 */
+    return 0;
 }
 unsigned getbits(unsigned x, int p, int n){
     return (x >> (p+1-n) & ~(~0 << n));
