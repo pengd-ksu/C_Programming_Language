a5fc2d66104273a65c9f93d33e05f26dcade946c
pengd-ksu <pengd@ksu.edu>
2023-03-30 06:42:46 -0400

    finished Exercise_4_7;

---
+++ Chapter4/Exercise_4_7/header.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+// for strlen
+#include <string.h>
+
+#define MAXSIZE 100
+
+int getch(void);
+void ungetch(int);
+void ungets(char []);
---
+++ Chapter4/Exercise_4_7/main.c
@@ -0,0 +1,17 @@
+#include "header.h"
+
+// reverse Polish calculator
+int main() {
+    int c;
+    char s[MAXSIZE] = "Remove this string.";
+    char t[MAXSIZE] = "A contrast to prev.";
+    printf("String s: [%s]\n", s);
+    printf("String t: [%s]\n", t);
+    ungets(s);
+    //printf("After ungets to string s: [%s]\n", s);
+    for (int i = strlen(t); (c = getch()) != EOF && i < MAXSIZE; i++) {
+        t[i] = c;
+    }
+    printf("New string t: [%s]\n", t);
+    return 0;
+}
