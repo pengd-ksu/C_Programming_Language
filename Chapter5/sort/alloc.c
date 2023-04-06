1721db5e6cf8a2b0830f48ca2b326559087f84d5
pengd-ksu <pengd@ksu.edu>
2023-04-06 00:19:17 -0400

    modified alloc in sort to unifiy the permitted maximum length of a single line;

--- Chapter5/sort/alloc.c
+++ Chapter5/sort/alloc.c
@@ -1,12 +1,13 @@
 #include "sortHeader.h"
 
-#define ALLOCSIZE 10000     /* size of available space */
-static char allocbuf[ALLOCSIZE];        /* storage for alloc */
-static char *allocp = allocbuf;         /* next free position */
+// storage for alloc
+static char allocbuf[MAXLEN];
+// next free position
+static char *allocp = allocbuf;
 
 // return pointer to n characters
 char *alloc(int n) {
-    if (allocbuf + ALLOCSIZE - allocp >= n) {   /* it fits */
+    if (allocbuf + MAXLEN - allocp >= n) {   /* it fits */
         allocp += n;
         return allocp - n;                       /* old p */
     } else                                      /* not enough room */
@@ -15,6 +16,6 @@ char *alloc(int n) {
 
 // free storage pointed to by p
 void afree(char *p) {
-    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+    if (p >= allocbuf && p < allocbuf + MAXLEN)
         allocp = p;
 }
