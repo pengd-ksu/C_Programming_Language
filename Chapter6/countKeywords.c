12547929531dd011ed98cc328c74d9371b5b1247
pengd-ksu <pengd@ksu.edu>
2023-04-08 20:22:24 -0400

    update till ch6;

--- Chapter6/countKeywords.c
+++ Chapter6/countKeywords.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+// To import isspace, isalpha, and isalnum
 #include <ctype.h>
 #include <string.h>
 
@@ -8,6 +9,8 @@
 char buf[BUFSIZE];  /* buffer for ungetch */
 int bufp = 0;       /* next free position in buf */
 
+// keytab is already sorted in increasing order,
+// that's why we could use binary search here.
 struct key {
     char *word;
     int count;
@@ -22,17 +25,71 @@ struct key {
     "unsigned", 0,
     "void", 0,
     "volatile", 0,
-    "void", 0,
     "while", 0
 };
 // NKEYS is the number of keywords in keytab
 #define NKEYS (sizeof keytab / sizeof(struct key))
 // Or: #define NKEYS (sizeof keytab / sizeof keytab[0])
 
-int getword(char *, int);
-int binsearch(char *, struct key *, int);
-//int getch(void);
-//void ungetch(int c);
+/* get a (possibly pushed back) character */
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* push character back on input */
+void ungetch(int c) {
+    if (bufp >= BUFSIZE) {
+        printf("ungetch: too many characters\n");
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+/* binsearch: find word in tab[0]...tab[n-1] */
+int binsearch(char *word, struct key tab[], int n) {
+    int cond;
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+        if ((cond = strcmp(word, tab[mid].word)) < 0) {
+            high = mid - 1;
+        } else if (cond > 0) {
+            low = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+/* getword: get next word or character from input */
+int getword(char *word, int lim) {
+    int c;
+    char *w = word;
+    while (isspace(c=getch())) {
+        ;
+    }
+    if (c != EOF) {
+        *w++ = c;
+    }
+    if (!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+    for (; --lim > 0; w++) {
+        // Will read next char to w even if next char
+        // isn't alpha nor number, since it's executed
+        // by (*w = getch()) as condition.
+        if(!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    }
+    *w = '\0';
+    return word[0];
+}
 
 /* count C keywords */
 int main() {
@@ -52,62 +109,4 @@ int main() {
         }
     }
     return 0;
-}
-
-/* binsearch: find word in tab[0]...tab[n-1] */
-int binsearch(char *word, struct key tab[], int n) {
-    int cond;
-    int low, high, mid;
-    low = 0;
-    high = n - 1;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if ((cond = strcmp(word, tab[mid].word)) < 0) {
-            high = mid - 1;
-        } else if (cond > 0) {
-            low = mid + 1;
-        } else {
-            return mid;
-        }
-    }
-    return -1;
-}
-
-/* getword: get next word or character from input */
-int getword(char *word, int lim) {
-    int c, getch(void);
-    void ungetch(int);
-    char *w = word;
-    while (isspace(c=getch())) {
-        ;
-    }
-    if (c != EOF) {
-        *w++ = c;
-    }
-    if (!isalpha(c)) {
-        *w = '\0';
-        return c;
-    }
-    for (; --lim > 0; w++) {
-        if(!isalnum(*w = getch())) {
-            ungetch(*w);
-            break;
-        }
-    }
-    *w = '\0';
-    return word[0];
-}
-
-/* get a (possibly pushed back) character */
-int getch(void) {
-    return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-/* push character back on input */
-void ungetch(int c) {
-    if (bufp >= BUFSIZE) {
-        printf("ungetch: too many characters\n");
-    } else {
-        buf[bufp++] = c;
-    }
 }
